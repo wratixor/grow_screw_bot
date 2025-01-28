b22a76fd3f65ab42ca9f99aa22d8b4b4227f1ca3
wratixor <wratixor@gmail.com>
2025-01-28 10:39:01 +0300

    fix numeric (6,2) -> (12,2)

--- db_utils/fix-7.sql
+++ db_utils/fix-7.sql
@@ -159,3 +159,146 @@ AS $function$
  END
 $function$
 ;
+
+CREATE OR REPLACE FUNCTION screw.s_screw_cut(i_chat_id bigint, i_user_id bigint, i_screw_id bigint, i_defuser_id bigint, i_defscrew_id bigint)
+ RETURNS text
+ LANGUAGE plpgsql
+ SECURITY DEFINER COST 1
+AS $function$
+ DECLARE
+
+  l_d20       numeric(4, 2) := screw.i_d20(i_user_id);
+  l_d20_def   numeric(4, 2) := screw.i_d20(i_defuser_id);
+  l_size      numeric(12, 2) := coalesce((select sizesm from screw.sc_screw where screw_id = i_screw_id), 0.0);
+  l_size_def  numeric(12, 2) := coalesce((select sizesm from screw.sc_screw where screw_id = i_defscrew_id), 0.0);
+
+  l_attack bool := ((l_d20_def + l_size_def) < (l_d20 + l_size));
+
+ BEGIN
+  IF l_attack THEN
+    update screw.sc_chat  set drop_screw  = i_defscrew_id       where chat_id  = i_chat_id;
+    update screw.sc_user  set growe_screw = screw.i_new_screw() where user_id  = i_defuser_id;
+    update screw.sc_screw set update_date = now()               where screw_id = i_defscrew_id;
+  END IF;
+
+  RETURN (
+    SELECT (u.username || ' засматривается на болт ' || ud.username || ' и решает его срезать! '
+            || l_d20 || ' + ' || l_size || 'см. против ' || l_d20_def ||  ' + ' || l_size_def || 'см.! '
+            || (case when l_attack then u.username || ' успешно срезает болт ' || ud.username || '! Быстрее хватай его в карман!'
+                                   else ud.username || ' успешно защищается своим болтом!' end))::text as res
+      from screw.sc_chat as c
+      join screw.sc_user as u  on  u.user_id = i_user_id
+      join screw.sc_user as ud on ud.user_id = i_defuser_id
+     where c.chat_id = i_chat_id
+     limit 1
+    );
+ END
+$function$
+;
+
+CREATE OR REPLACE FUNCTION screw.s_screw_breack(i_chat_id bigint, i_user_id bigint, i_screw_id bigint, i_defuser_id bigint, i_defscrew_id bigint)
+ RETURNS text
+ LANGUAGE plpgsql
+ SECURITY DEFINER COST 1
+AS $function$
+ DECLARE
+
+  l_d20       numeric(4, 2) := screw.i_d20(i_defuser_id);
+  l_d20_def   numeric(4, 2) := screw.i_d20(i_user_id);
+  l_size      numeric(12, 2) := (select sizesm from screw.sc_screw where screw_id = i_screw_id);
+  l_size_def  numeric(12, 2) := (select sizesm from screw.sc_screw where screw_id = i_defscrew_id);
+
+  l_half_size numeric(12, 2) := 0.0;
+
+  l_looser       bigint     := 0;
+  l_looser_screw bigint     := 0;
+
+ BEGIN
+  IF (l_d20_def + l_size_def) > (l_d20 + l_size) THEN
+    l_looser := i_defuser_id;
+    l_looser_screw := i_defscrew_id;
+  ELSE
+    l_looser := i_user_id;
+    l_looser_screw := i_screw_id;
+  END IF;
+
+  l_half_size := (select sizesm / 2 from screw.sc_screw where screw_id = l_looser_screw);
+  update screw.sc_chat set drop_screw = screw.i_new_screw(l_half_size) where chat_id = i_chat_id;
+  update screw.sc_screw set sizesm = l_half_size, update_date = now() where screw_id = l_looser_screw;
+
+  RETURN (
+    SELECT 'На арене "' || c.chat_title || '" ' || u.username || ' решает смахнуться болтами с ' || ud.username
+            || '! ' || l_d20 || ' + ' || l_size || 'см. против ' || l_d20_def ||  ' + ' || l_size_def
+            || 'см.! Болт ' || ul.username || ' разлетается пополам! Обломок ' || l_half_size || 'см. можно забрать!'
+      from screw.sc_chat as c
+      join screw.sc_user as u  on  u.user_id = i_user_id
+      join screw.sc_user as ud on ud.user_id = i_defuser_id
+      join screw.sc_user as ul on ul.user_id = l_looser
+     where c.chat_id = i_chat_id
+     limit 1
+    );
+ END
+$function$
+;
+
+CREATE OR REPLACE FUNCTION screw.s_screw_attack(i_chat_id bigint, i_user_id bigint, i_screw_id bigint, i_defuser_id bigint, i_defscrew_id bigint)
+ RETURNS text
+ LANGUAGE plpgsql
+ SECURITY DEFINER COST 1
+AS $function$
+ DECLARE
+
+  l_d20       numeric(4, 2) := screw.i_d20(i_user_id);
+  l_d20_def   numeric(4, 2) := screw.i_d20(i_defuser_id);
+  l_size      numeric(12, 2) := (select sizesm from screw.sc_screw where screw_id = i_screw_id);
+  l_size_def  numeric(12, 2) := (select sizesm from screw.sc_screw where screw_id = i_defscrew_id);
+
+  l_half_size numeric(12, 2) := 0.0;
+
+  l_win boolean := ((l_d20_def + l_size_def) < (l_d20 + l_size));
+
+  l_catch_screw bigint := (select catch_screw from screw.sc_user as c where c.user_id = i_defuser_id);
+  l_screw_size numeric(12, 2) := (select sizesm from screw.sc_screw where screw_id = l_catch_screw);
+
+ BEGIN
+  IF l_win THEN
+    update screw.sc_user set catch_screw = l_catch_screw where user_id = i_user_id;
+    update screw.sc_user set catch_screw = null          where user_id = i_defuser_id;
+  END IF;
+
+
+  RETURN (
+    SELECT 'На арене "' || c.chat_title || '" ' || u.username || ' бессовестно нападает на ' || ud.username
+            || '! ' || l_d20 || ' + ' || l_size || 'см. против ' || l_d20_def ||  ' + ' || l_size_def
+            || 'см.! ' || u.username || (case when l_win then ' успешно отбирает ' else ' не удаётся отобрать ' end)
+            || 'у ' || ud.username || ' обломок ' || l_screw_size || 'см.!'
+      from screw.sc_chat as c
+      join screw.sc_user as u  on  u.user_id = i_user_id
+      join screw.sc_user as ud on ud.user_id = i_defuser_id
+     where c.chat_id = i_chat_id
+     limit 1
+    );
+ END
+$function$
+;
+
+CREATE OR REPLACE FUNCTION screw.i_new_screw(i_size numeric)
+ RETURNS bigint
+ LANGUAGE plpgsql
+ SECURITY DEFINER COST 1
+AS $function$
+ DECLARE
+  l_screw_id bigint := 0;
+  l_size numeric(12, 2) := i_size;
+
+ BEGIN
+
+  l_screw_id := nextval('screw.screw_sq'::regclass);
+  insert into screw.sc_screw(screw_id, sizesm) values (l_screw_id, l_size);
+
+  RETURN (
+    select l_screw_id
+  );
+ END
+$function$
+;
